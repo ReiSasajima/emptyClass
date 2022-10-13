cb233f640f2cc71f0157f304b1c2be4c9e951c93
Rei Sasajima <sasajimarei@gmail.com>
2022-10-13 11:29:13 +0900

    add get request and post request medthods

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import sqlite3
 
 app = Flask(__name__)
 
-# # データベースの接続
-# conn = sqlite3.connect('./ranking.db', check_same_thread=False)
-# cur = conn.cursor()
+# データベースの接続
+conn = sqlite3.connect('./syllabus.db')
+cur = conn.cursor()
+
 @app.route('/', methods=['POST', 'GET'])
 def search():
   if request.method == 'GET':
@@ -18,4 +19,11 @@ def search():
     # 時限を取得
     time = request.form.get('time')
     print(campus, day, time)
+
+    key = "day" + "time"
+    print(key)
+    # result = cur.execute('''SELECT "教室" 
+    #                       FROM syllabus
+    #                       WHERE "授業時間" LIKE ?''', ('%'+key+'%',),)
+    # print(result)
 
